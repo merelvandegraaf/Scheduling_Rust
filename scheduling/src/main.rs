3b49b23e547b220c8550edf119a011f64cbb93ab
Merel <merelvandegraaf@gmail.com>
2021-05-29 19:31:23 +0200

    Alle code compleet
    Zit ergens nog een infinite loop

--- scheduling/src/main.rs
+++ scheduling/src/main.rs
@@ -3,16 +3,17 @@ mod modules;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let arg = &args[1];
-    let text_file = TextFile::new(arg.to_string());
-    //text_file.execute();
+    let mut text_file = TextFile::new(arg.to_string());
+    text_file.execute();
 }
 
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 pub struct TextFile {
     file_name: String,
-    //currentJobs: Vec<Job>,
-    currentTasks: Vec<modules::task::Task>,
+    current_jobs: Vec<modules::task::Job>,
+    current_tasks: Vec<modules::task::Task>,
     current_getal: Vec<u16>,
     current_job_progress: u16,
     current_number_of_jobs: u16,
@@ -25,8 +26,8 @@ impl TextFile {
     pub fn new(file_name: String) -> Self {
         Self {
             file_name,
-            //currentJobs: Vec::with_capacity(50),
-            currentTasks: Vec::with_capacity(50),
+            current_jobs: Vec::with_capacity(50),
+            current_tasks: Vec::with_capacity(50),
             current_getal: Vec::with_capacity(4),
             current_job_progress: 0,
             current_number_of_jobs: 0,
@@ -36,57 +37,255 @@ impl TextFile {
         }
     }
 
-//     pub fn execute(&self) {
-//         if let Ok(lines) = Self::read_lines(&self.file_name) {
-//             // Consumes the iterator, returns an (Optional) String
-//             for line in lines {
-//                 if let Ok(ip) = line {
-//                     let i;
-//                     let char_vec: Vec<char> = ip.chars().collect();
-//                     for character in 0..char_vec.len()
-//                     {
-//                         i = char_vec[character];
-//                         if (!checkForTasks(character, char_vec))
-//                         {
-//                             break;
-//                         }
-//                         //Checken of er een JobShop wordt gevuld of gezocht
-//                         if (!makingJobShop)
-//                         {
-//                             checkForNewJobShop(static_cast<unsigned short>(i));
-//                         }
-//                         else
-//                         {
-//                             fillJobShop(static_cast<unsigned short>(i));
-//                         }
-//                     }
-//                     changeJobShopProgress();
-//                 }
-//             }
-//         }
-//     }
+    pub fn execute(&mut self) {
+        if let Ok(lines) = TextFile::read_lines(&self.file_name) {
+            // Consumes the iterator, returns an (Optional) String
+            for line in lines {
+                if let Ok(ip) = line {
+                    let mut i;
+                    let char_vec: Vec<char> = ip.chars().collect();
+                    for character in 0..char_vec.len()
+                    {
+                        i = &char_vec[character];
+                        if !self.check_for_tasks(character as u16, char_vec.clone())
+                        {
+                            break;
+                        }
+                        //Checken of er een JobShop wordt gevuld of gezocht
+                        if !self.making_job_shop
+                        {
+                            self.check_for_new_jobshop(i);
+                        }
+                        else
+                        {
+                            self.fill_jobshop(i);
+                        }
+                    }
+                    self.change_jobshop_progress();
+                }
+            }
+        }
+    }
 
-//     fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<std::fs::File>>>
-//     where
-//         P: AsRef<std::path::Path>,
-//     {
-//         let file = std::fs::File::open(filename)?;
-//         Ok(io::BufReader::new(file).lines())
-//     }
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<std::fs::File>>>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        let file = std::fs::File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
 
-//    fn checkForTasks(index: u16,  line: Vec<char>) -> bool
-// {
-// 	let tasks_remaining = false;
-// 	std::string character;
-// 	for (signed short i = line.size() - 1; i >= index; --i)
-// 	{
-// 		character = line.at(i);
-// 		if (std::regex_search(character, std::regex("\\d+")))
-// 		{
-// 			tasksRemaining = true;
-// 			break;
-// 		}
-// 	}
-// 	return tasksRemaining;
-// }
+   pub fn check_for_tasks(&self, index: u16,  line: Vec<char>) -> bool{
+       let mut tasks_remaining = false;
+       let mut character;
+
+       for i in (index..(line.len() as u16)).rev(){
+           character = line[i as usize];
+           if character.is_digit(10){
+               tasks_remaining = true;
+               break;
+           }
+       }
+       return tasks_remaining;
+    }
+
+    pub fn calculate_current_getal(&mut self) -> u16{
+        let mut factor = 1;
+        let mut getal = 0;
+
+        for i in (0..(self.current_getal.len())).rev(){
+            if factor == 0{
+                getal += self.current_getal[i];
+            }else{
+                getal += self.current_getal[i] * factor;
+            }
+            factor = factor * 10;
+        }
+        self.current_getal.clear();
+        return getal;
+    }
+
+    pub fn check_white_space(&mut self, i: &char) -> bool{
+        if i.is_digit(10){
+            self.current_getal.push(*i as u16 - 48);
+        }else if self.current_getal.len() > 0 &&
+        i.is_ascii_whitespace(){
+            return true;
+        }
+        return false;
+    }
+
+    pub fn check_for_new_jobshop(&mut self, i: &char){
+        if self.check_white_space(i) && self.current_number_of_jobs == 0{
+            self.current_number_of_jobs = self.calculate_current_getal();
+        }
+    }
+
+    pub fn fill_jobshop(&mut self, i: &char){
+        if self.check_white_space(i){
+            if self.current_machine_id == self.current_number_of_machines{
+                self.current_machine_id = self.calculate_current_getal();
+            }else{
+                let getal = self.calculate_current_getal();
+                self.current_tasks.push(modules::task::Task::new(self.current_machine_id, getal));
+                self.current_machine_id = self. current_number_of_machines;
+            }
+        }
+    }
+
+    pub fn change_jobshop_progress(&mut self){
+        if !self.current_jobs.is_empty(){
+            println!("nr of job: {0} nr of tasks: {1}", self.current_jobs[0].get_job_id(), self.current_jobs[0].get_tasks().len());
+        }
+        if !self.making_job_shop && self.current_number_of_jobs != 0{
+            self.current_number_of_machines = self.calculate_current_getal();
+            self.current_machine_id = self.current_number_of_machines;
+            self.making_job_shop = true;
+        }else if self.making_job_shop{
+            let getal = self.calculate_current_getal();
+            self.current_tasks.push(modules::task::Task::new(self.current_machine_id, getal));
+            self.current_machine_id = self.current_number_of_machines;
+            self.current_jobs.push(modules::task::Job::new(self.current_job_progress,self.current_tasks.clone()));
+            self.current_tasks.clear();
+            self.current_job_progress +=1;
+
+            if self.current_job_progress == self.current_number_of_jobs{
+                let mut jb = JobShop::new(self.current_jobs.clone(), self.current_number_of_machines);
+                jb.schedule();
+                println!();
+                
+                self.current_jobs.clear();
+                self.current_number_of_jobs = 0;
+                self.current_number_of_machines = 0;
+                self.current_job_progress = 0;
+                self. making_job_shop = false;
+            }
+        }
+    }
+}
+
+
+pub struct JobShop{
+    jobs : Vec<modules::task::Job>,
+    machine_status: HashMap< u16, u16>,
+	maximum_duration: u16,
+}
+
+impl JobShop{
+    pub fn new(some_jobs: Vec<modules::task::Job>, number_of_machines: u16) -> Self{
+        let mut temp_map = HashMap::new();
+        for x in 0..(number_of_machines+1){
+            temp_map.insert(x, some_jobs.len() as u16);
+        }
+
+        let mut temp_object = JobShop {
+            jobs : some_jobs,
+            machine_status: temp_map,
+            maximum_duration: 0,
+        };
+        temp_object.change_latest_start_times();
+        return temp_object;
+    }
+
+    pub fn schedule(&mut self){
+        let mut current_time = 0;
+        while self.check_all_jobs_completed() == false{
+            self.calculate_progress(current_time);
+            current_time = current_time+1;
+        }
+        self.print_output();
+    }
+
+    pub fn calculate_progress(&mut self, current_time: u16){
+        for machine_index in 0..self.machine_status.len(){
+            if self.machine_status[&(machine_index as u16)] != (self.jobs.len() as u16){
+                let machine_job_id = self.machine_status[&(machine_index as u16)];
+                self.jobs[machine_job_id as usize].get_first_open_task().up_current_progress();
+
+                if self.jobs[machine_job_id as usize].get_first_open_task().get_current_progress() > 
+                self.jobs[machine_job_id as usize].get_first_open_task().get_duration(){
+                    self.jobs[machine_job_id as usize].get_first_open_task().set_task_completed(true);
+                    self.recalculate_total_durations(current_time);
+                    self.change_latest_start_times();
+
+                    self.set_machine_status(machine_index as u16, self.jobs.len() as u16);
+
+                    if self.jobs[machine_job_id as usize].get_first_open_task() == self.jobs[machine_job_id as usize].get_end_task(){
+                        self.jobs[machine_job_id as usize].set_end_time(current_time);
+                    }
+                }
+            }
+        }
+
+        for machine_index in 0..self.machine_status.len(){
+            if self.machine_status[&(machine_index as u16)] != (self.jobs.len() as u16){
+                let mut current_slack = self.maximum_duration + 1;
+                let mut current_job_id = self.jobs.len() as u16;
+
+                for j in &self.jobs{
+                    if j.get_first_open_task() != j.get_end_task() &&
+                    j.get_first_open_task().get_machine_id() == machine_index as u16 &&
+                    j.get_first_open_task().get_latest_start_time() - current_time < current_slack{
+                        current_job_id = j.get_job_id();
+                        current_slack = j.get_first_open_task().get_latest_start_time() - current_time;
+                    }
+                }
+
+                if current_job_id != self.jobs.len() as u16{
+                    self.set_machine_status(machine_index as u16, current_job_id);
+                    self.jobs[current_job_id as usize].get_first_open_task().up_current_progress();
+
+                    if self.jobs[current_job_id as usize].get_first_open_task() == self.jobs[current_job_id as usize].get_first_task(){
+                        self.jobs[current_job_id as usize].set_start_time(current_time);
+                    }
+                }
+            }
+
+        }
+    }
+
+    pub fn recalculate_total_durations(&mut self, current_time: u16){
+        for j in &mut self.jobs{
+            j.calculate_total_duration(current_time);
+        }
+    }
+
+    pub fn change_latest_start_times(&mut self){
+        let old_maximum_duration =  self.maximum_duration;
+
+        for j in &self.jobs{
+            if j.get_first_open_task() != j.get_end_task() &&
+            j.get_total_duration() > self.maximum_duration{
+                self.maximum_duration = j.get_total_duration();
+            }
+        }
+
+        if self.maximum_duration > old_maximum_duration{
+            for j in &mut self.jobs{
+                if j.get_first_open_task() != j.get_end_task(){
+                    j.set_slack((self.maximum_duration - j.get_total_duration()) as i16);
+                    j.set_latest_start_times();
+                }
+            }
+        }
+    }
+
+    pub fn print_output(&self){
+        for j in &self.jobs{
+            println!("{0} {1} {2}", j.get_job_id(), j.get_start_time(), j.get_end_time());
+        }  
+    }
+
+    pub fn check_all_jobs_completed(&mut self) -> bool{
+        for j in &mut self.jobs{
+            if j.get_first_open_task() != j.get_end_task(){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    pub fn set_machine_status(&mut self, machine_id: u16, job_id: u16){
+        self.machine_status.insert(machine_id, job_id);
+    }
 }
--- scheduling/src/modules/task.rs
+++ scheduling/src/modules/task.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Clone, Copy, Eq)]
 pub struct Task {
     machine_id: u16,
     duration: u16,
@@ -18,35 +18,167 @@ impl Task {
         }
     }
 
-    pub fn get_machine_id(&self) -> &u16 {
-        &self.machine_id
+    pub fn get_machine_id(&self) -> u16 {
+        self.machine_id
     }
 
-    pub fn get_duration(&self) -> &u16 {
-        &self.duration
+    pub fn get_duration(&self) -> u16 {
+        self.duration
     }
 
-    pub fn get_current_progress(&self) -> &u16 {
-        &self.current_progress
+    pub fn get_current_progress(&self) -> u16 {
+        self.current_progress
     }
 
     pub fn up_current_progress(&mut self) {
         self.current_progress = self.current_progress + 1;
     }
 
-    pub fn is_task_completed(&self) -> &bool {
-        &self.task_completed
+    pub fn is_task_completed(&self) -> bool {
+        self.task_completed
     }
 
     pub fn set_task_completed(&mut self, new_task_completed: bool) {
         self.task_completed = new_task_completed;
     }
 
-    pub fn get_latest_start_time(&self) -> &u16 {
-        &self.latest_start_time
+    pub fn get_latest_start_time(&self) -> u16 {
+        self.latest_start_time
     }
 
     pub fn set_latest_start_time(&mut self, new_latest_start_time: u16) {
         self.latest_start_time = new_latest_start_time;
+    }
+}
+
+#[derive(PartialEq, Clone, Eq)]
+pub struct Job{
+    job_id: u16,
+    tasks: Vec<Task>,
+    slack: i16,
+    total_duration: u16,
+    start_time: u16,
+    end_time: u16,
+}
+
+impl Job{
+    pub fn new(a_job_id: u16, some_tasks: Vec<Task>) -> Self {
+        let mut temp_total_duration = 0;
+        for t in &some_tasks{
+            temp_total_duration += t.get_duration();
+        }
+        Self {
+        job_id: a_job_id,
+        tasks: some_tasks,
+        slack: 0,
+        total_duration: temp_total_duration,
+        start_time: 0,
+        end_time: 0,
+        }
+    }
+
+    pub fn get_first_open_task(&self) -> Task {
+        let mut a_task = self.tasks[0];
+        for t in &self.tasks{
+            if t.is_task_completed() == true {
+                a_task = *t;
+               break;
+            }
+        } 
+        return a_task;
+    }
+
+    pub fn set_latest_start_times(&mut self){
+        let mut sub_total = self.total_duration;
+	    for t in &self.tasks
+	    {
+		    if !t.is_task_completed()
+		    {
+			    sub_total -= t.get_duration();
+		    }
+	    }
+	    for  t in &mut self.tasks
+	    {
+		    if !t.is_task_completed()
+		    {
+			    t.set_latest_start_time(sub_total + (self.slack as u16));
+			    sub_total += t.get_duration();
+		    }
+	    }
+    }
+
+    pub fn calculate_total_duration(&mut self, current_time: u16){
+        if self.get_first_open_task().get_latest_start_time() < current_time
+	    {
+		    //Reset de totalDuration.
+		    self.total_duration = 0;
+		    for t in &self.tasks
+		    {
+			    self.total_duration += t.get_duration();
+		    }
+
+		    //Bereken de tijd van de tasks die klaar zijn.
+		    let mut worked_time = 0;
+		    for t in &self.tasks
+		    {
+			    if t.is_task_completed()
+			    {
+				    worked_time += t.get_duration();
+			    }
+		    }
+		    //Verhoogt de totalDuration met de tijd die extra nodig was om de voltooide tasks af te krijgen
+		    if current_time >= worked_time
+		    {
+		    	self.total_duration += current_time - worked_time;
+	    	}
+	    }
+    }
+
+    pub fn get_end_task(&self) -> Task{
+        return self.tasks[self.tasks.len()-1]
+    }
+
+    pub fn get_job_id(&self) -> u16{
+        return self.job_id;
+    }
+
+    pub fn set_job_id(&mut self, a_job_id: u16){
+        self.job_id = a_job_id;
+    }
+
+    pub fn get_slack(&self) -> i16{
+        return self.slack;
+    }
+
+    pub fn set_slack(&mut self, a_slack: i16){
+        self.slack = a_slack;
+    }
+
+    pub fn get_total_duration(&self) -> u16{
+        return self.total_duration;
+    }
+
+    pub fn get_start_time(&self) -> u16{
+        return self.start_time;
+    }
+
+    pub fn set_start_time(&mut self, a_start_time: u16){
+        self.start_time = a_start_time;
+    }
+
+    pub fn get_end_time(&self) -> u16{
+        return self.end_time;
+    }
+
+    pub fn set_end_time(&mut self, a_end_time: u16){
+        self.end_time = a_end_time;
+    }
+
+    pub fn get_first_task(&self) -> Task{
+        return self.tasks[0];
+    }
+
+    pub fn get_tasks(&self) -> &Vec<Task>{
+        return &self.tasks;
     }
 }
